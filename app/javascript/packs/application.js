4d5f429e265ad8b9109989b40ac9165105aa8e36
Einar Jonsson <einarjonsson@mac.com>
2019-04-06 17:56:39 +0100

    initialise install prompt

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,32 +5,11 @@ import { initPokeSuggestions } from '../components/initPokeSuggestions';
 import { initPokesIndexTabs } from '../components/initPokesIndexTabs';
 import { initNavbarScroll } from '../components/initNavbarScroll';
 import { updateLastSeenPokeOnExit } from '../components/updateLastSeenPokeOnExit';
+import { initInstallPrompt } from '../components/initInstallPrompt';
 
 initNavbarScroll();
 initPokeSuggestions();
 initAutoResizeTextArea();
 initPokesIndexTabs();
 updateLastSeenPokeOnExit();
-
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  console.log(e);
-
-  document.querySelector('body').insertAdjacentHTML('beforeend', `
-    <button class="btn btn-success">Install</button>
-    `)
-
-  deferredPrompt = e;
-
-  document.querySelector('button.btn-success').addEventListener('click', (event) => {
-    deferredPrompt.prompt();
-  });
-});
-
-
-
+initInstallPrompt();
